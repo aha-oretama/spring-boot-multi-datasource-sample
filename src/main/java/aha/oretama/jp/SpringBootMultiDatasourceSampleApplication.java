43a0ac9cea846f663b2471ef422ccb4005196fa3
yasufumi sekine <yasufumi_sekine@r.recruit.co.jp>
2017-01-04 15:20:27 +0900

    initial commit

---
+++ src/main/java/aha/oretama/jp/SpringBootMultiDatasourceSampleApplication.java
@@ -0,0 +1,12 @@
+package aha.oretama.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiDatasourceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiDatasourceSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/aha/oretama/jp/SpringBootMultiDatasourceSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package aha.oretama.jp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMultiDatasourceSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
